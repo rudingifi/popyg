6117195b627468db0ed85763e4a3198af9bb273a
rudingifi <mark.pavliv15@gmail.com>
2025-05-02 13:40:07 +0200

    hello file for netlify functions added

---
+++ netlify/functions/hello.js
@@ -0,0 +1,12 @@
+exports.handler = async function(event, context) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Hello from Netlify Functions!",
+            timestamp: new Date().toISOString()
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+}; 
